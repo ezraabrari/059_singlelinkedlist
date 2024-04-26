b56a2ecaaedc11034bd3ffb9a58980962ba632c1
ezraabrari <ezraabrari13@gmail.com>
2024-04-26 14:01:03 +0700

    jikalau NIM yang sudah dimasukin sudah ada

--- 059_singlelinkedlist/059_singlelinkedlist.cpp
+++ 059_singlelinkedlist/059_singlelinkedlist.cpp
@@ -19,6 +19,13 @@ void addNode() {
 	cin >> nama;
 	nodeBaru->noMhs = nim;
 	nodeBaru->name = nama;
+	if (START == NULL || nim <= START->noMhs) {
+		cout << "NIM sudah ada" << endl;
+		return;
+	}
 
+	nodeBaru->next = START;
+	START = nodeBaru;
+	return;
 
 }
