fdd3d1f5cb342c255c79b885d4f546afba3eb0f5
ezraabrari <ezraabrari13@gmail.com>
2024-04-26 14:17:08 +0700

    membuat codingan searchNode

--- 059_singlelinkedlist/059_singlelinkedlist.cpp
+++ 059_singlelinkedlist/059_singlelinkedlist.cpp
@@ -2,30 +2,72 @@
 using namespace std;
 
 struct Node {
-	int noMhs;
-	string name;
-	Node* next;
+    int noMhs;
+    string name;
+    Node* next;
 };
 
 Node* START = NULL;
 
 void addNode() {
-	int nim;
-	string nama;
-	Node* nodeBaru = new Node();
-	cout << "Masukkan NIM : ";
-	cin >> nim;
-	cout << "Masukkan Nama : ";
-	cin >> nama;
-	nodeBaru->noMhs = nim;
-	nodeBaru->name = nama;
-	if (START == NULL || nim <= START->noMhs) {
-		cout << "NIM sudah ada" << endl;
-		return;
-	}
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+    cout << "Masukkan Nama: ";
+    cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
 
-	nodeBaru->next = START;
-	START = nodeBaru;
-	return;
+    if (START == NULL || nim <= START->noMhs) {
+        if (START != NULL && nim == START->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
 
+        nodeBaru->next = START;
+        START = nodeBaru;
+        return;
+    }
+
+    Node* previous = START;
+    Node* current = START;
+
+    while ((current != NULL) && (nim >= current->noMhs))
+    {
+        if (nim == current->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+
+    nodeBaru->next = current;
+    previous->next = nodeBaru;
+}
+
+bool searchNode(int nim, Node* current, Node* previous) {
+    previous = START;
+    current = START;
+    while (current != NULL && nim > current->noMhs)
+    {
+        previous = current;
+        current = current->next;
+    }
+    if (current == NULL)
+    {
+        return false;
+    }
+    else if (current->noMhs == nim)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
